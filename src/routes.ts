e85ecc30079fa3999acb06bfa6667eab61dec483
macedo-developer <macedorenata.developer@gmail.com>
2020-06-02 22:18:08 -0300

    Separating the functions and structuring the folders

--- src/routes.ts
+++ src/routes.ts
@@ -1,57 +1,14 @@
-import express, { response } from "express";
+import express from "express";
 
-import knex from "./database/connection";
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
 const routes = express.Router();
 
-routes.get("/items", async (request, response) => {
-  const items = await knex("items").select("*");
+const pointController = new PointsController();
+const itemsController = new ItemsController();
 
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/upload/${item.image}`,
-    };
-  });
+routes.get("/items", itemsController.index);
 
-  return response.json(serializedItems);
-});
-
-routes.post("/points", async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = request.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx("point").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const pointItems = items.map((item_id: Number) => {
-    return {
-      item_id,
-      point_id: insertedIds[0],
-    };
-  });
-
-  await trx("point_items").insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.post("/points", pointController.create);
 export default routes;
