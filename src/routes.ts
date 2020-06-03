3a769426ad0a9fdb89c60dd6a25b1fa28ee1fd36
macedo-developer <macedorenata.developer@gmail.com>
2020-06-02 21:59:46 -0300

    Create router insert point and items of point

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 
 import knex from "./database/connection";
 
@@ -9,6 +9,7 @@ routes.get("/items", async (request, response) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/upload/${item.image}`,
     };
@@ -17,4 +18,40 @@ routes.get("/items", async (request, response) => {
   return response.json(serializedItems);
 });
 
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("point").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id: insertedIds[0],
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return response.json({ success: true });
+});
 export default routes;
